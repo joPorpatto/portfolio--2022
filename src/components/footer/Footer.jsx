d7e4d639c39e9bf0cde3ad0d0a73e489bcfd13bf
joPorpatto <joel.porpatto@gmail.com>
2022-05-01 13:24:31 -0300

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,14 +7,14 @@ export const Footer = () => {
   
   return (
     <footer>        
-          <a href='#' className='footer__logo'>WEB</a>
+          <a href='#' className='footer__logo'>Porfolio 2022</a>
 
           <ul className='permalinks'>
             <li><a href='#'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#experience'>Experience</a></li>
+            <li><a href='#about'>Sobre mi</a></li>
+            <li><a href='#experience'>Experiencia</a></li>
             <li><a href='#portfolio'>Portfolio</a></li>
-            <li><a href='#contact'>Contact</a></li>
+            <li><a href='#contact'>Contacto</a></li>
           </ul>
 
           <div className="footer__socials">
@@ -23,7 +23,7 @@ export const Footer = () => {
           </div>
 
       <div className="footer__copy">
-        <small>&copy; WEB asdasdas</small>
+        <small>&copy; Joel Porpatto WebSite</small>
       </div>  
         
       
